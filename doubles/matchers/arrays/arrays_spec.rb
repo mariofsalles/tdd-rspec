f85cb26678ecfa9c66821d819895a2526ad00fd6
Mario Salles <mariofsalles@gmail.com>
2019-02-17 19:18:03 +0000

    Tag filter, custom matchers

--- doubles/matchers/arrays/arrays_spec.rb
+++ doubles/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]) do
+describe Array.new([1,2,3]), "Array",type: 'collection' do
   it '# include' do # só funciona para elementos
     expect(subject).to include(2)
   end
@@ -9,7 +9,7 @@ describe Array.new([1,2,3]) do
     expect(subject).to exclude(5)
   end
 
-  it '# macth array' do # para o array exato
+  it '# match array' do # para o array exato
     expect(subject).to match_array([1,2,3]) 
   end
 
---
+++ doubles/matchers/costum/custom_spec.rb
@@ -0,0 +1,17 @@
+RSpec::Matchers.define :be_a_multiple_of do |expected|
+  # expected == 3
+  # actual == subject
+  match do |actual|
+    actual % expected == 0
+  end
+  failure_message do |actual|
+    "expected that #{actual} would be a multiple of #{expected}"
+  end
+  description do
+  "be a multiple of #{expected}"
+  end
+end
+
+describe 18, ' :Custom Matcher' do
+  it { is_expected.to be_a_multiple_of(3)}
+end
